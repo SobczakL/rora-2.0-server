7aa220cd0110daecce98213fad15a5b472480a39
Lucas sobczak <lucassobczak3@gmail.com>
2025-02-06 23:41:39 -0500

    testing external api

--- controllers/data.go
+++ controllers/data.go
@@ -2,19 +2,53 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
-type Response struct {
-	Message string `json:"message"`
+type FeedResponse struct {
+	Feeds []struct {
+		OnestopID string `json:"onestop_id"`
+		Name      string `json:"name"`
+		URL       string `json:"url"`
+	} `json:"feeds"`
 }
 
 func GetData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	// Example response data
-	response := Response{Message: "Hello from Go controller!"}
+	apiKey := os.Getenv("TRANSITLAND_API_KEY")
+	if apiKey == "" {
+		http.Error(w, "API key not set", http.StatusInternalServerError)
+		log.Println("TRANSITLAND_API_KEY is missing")
+		return
+	}
 
-	// Return the response as JSON
-	json.NewEncoder(w).Encode(response)
+	url := fmt.Sprintf("https://transit.land/api/v2/rest/feeds?apikey=%s", apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(w, "Failed to fetch data from Transitland", http.StatusBadGateway)
+		log.Println("Error making API request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read API response", http.StatusBadGateway)
+		log.Println("Error reading response body:", err)
+		return
+	}
+
+	var feedResponse FeedResponse
+	if err := json.Unmarshal(body, &feedResponse); err != nil {
+		http.Error(w, "Failed to parse API response", http.StatusBadGateway)
+		log.Println("Error decoding JSON:", err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(feedResponse)
 }
